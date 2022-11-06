863f220ba060fd93283a6ff9ceb89816ce3d90e1
Thanos Trifonis <ath.trifonis@gmail.com>
2022-11-06 10:59:36 +0200

    init commit

---
+++ RubikCube/src/RubikCube.java
@@ -0,0 +1,74 @@
+public class RubikCube {
+    private RubikSide front;
+    private RubikSide back;
+    private RubikSide left;
+    private RubikSide right;
+    private RubikSide top;
+    private RubikSide bottom;
+    private int size;
+
+    public RubikCube(int size) {
+        this.size = size;
+        this.front = new RubikSide(size, 1);
+        this.back = new RubikSide(size, 2);
+        this.left = new RubikSide(size, 3);
+        this.right = new RubikSide(size, 4);
+        this.top = new RubikSide(size, 5);
+        this.bottom = new RubikSide(size, 6);
+    }
+
+    public RubikSide getFront() {
+        return this.front;
+    }
+
+    public RubikSide getBack() {
+        return this.back;
+    }
+
+    public RubikSide getLeft() {
+        return this.left;
+    }
+
+    public RubikSide getRight() {
+        return this.right;
+    }
+
+    public RubikSide getTop() {
+        return this.top;
+    }
+
+    public RubikSide getBottom() {
+        return this.bottom;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public void turnRow(int row, int direction) {
+        int[] rowCopy = getFront().getRow(row);
+        // Right turn.
+        if (direction == 1) {
+            getFront().setRow(row, getLeft().getRow(row));
+            getLeft().setRow(row, getBack().getRow(row));
+            getBack().setRow(row, getRight().getRow(row));
+            getRight().setRow(row, rowCopy);
+            if (row == 0) {
+                getTop().rotateAnticlockwise();
+            } else if(row == getSize() - 1) {
+                getBottom().rotateClockwise();
+            }
+            // Left turn.
+        } else if (direction == -1) {
+            getFront().setRow(row, getRight().getRow(row));
+            getRight().setRow(row, getBack().getRow(row));
+            getBack().setRow(row, getLeft().getRow(row));
+            getLeft().setRow(row, rowCopy);
+            if (row == 0) {
+                getTop().rotateClockwise();
+            } else if(row == getSize() - 1) {
+                getBottom().rotateAnticlockwise();
+            }
+        }
+    }
+}
---
+++ RubikCube/src/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void num1() {
+        RubikCube cube = new RubikCube(3);
+        System.out.println(rub);
+    }
+}
