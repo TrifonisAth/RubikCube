5dd655a173b23adf1ef3b233636ba4381f148752
Thanos Trifonis <ath.trifonis@gmail.com>
2022-11-06 18:33:42 +0200

    Adds printCube

--- RubikCube/src/RubikCube.java
+++ RubikCube/src/RubikCube.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class RubikCube {
     private RubikSide front;
     private RubikSide back;
@@ -10,9 +12,9 @@ public class RubikCube {
     public RubikCube(int size) {
         this.size = size;
         this.front = new RubikSide(size, 1);
-        this.back = new RubikSide(size, 2);
-        this.left = new RubikSide(size, 3);
-        this.right = new RubikSide(size, 4);
+        this.right = new RubikSide(size, 2);
+        this.back = new RubikSide(size, 3);
+        this.left = new RubikSide(size, 4);
         this.top = new RubikSide(size, 5);
         this.bottom = new RubikSide(size, 6);
     }
@@ -71,6 +73,30 @@ public class RubikCube {
                 getBottom().rotateAnticlockwise();
             }
         }
+    }
+
+    // Print cube sides.
+    public void printCube() {
+        String[][] cube = new String[getSize() * 2 + 1][getSize() * 2 + 1];
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            str.append("\t".repeat(size/2 +1).concat("\t").concat(Arrays.toString(getTop().getRow(i))));
+            str.append("\n");
+        }
+        str.append("\n");
+        for (int i = 0; i < size; i++){
+            str.append(Arrays.toString(getLeft().getRow(i))).append("\t");
+            str.append(Arrays.toString(getFront().getRow(i))).append("\t");
+            str.append(Arrays.toString(getRight().getRow(i))).append("\t");
+            str.append(Arrays.toString(getBack().getRow(i)));
+            str.append("\n");
+        }
+        str.append("\n");
+        for (int i = 0; i < size; i++) {
+            str.append("\t".repeat(size/2 + 1).concat("\t").concat(Arrays.toString(getBottom().getRow(i))));
+            str.append("\n");
+        }
+        System.out.println(str);
     }
 
 
