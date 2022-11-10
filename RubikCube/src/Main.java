15050d2fa9cf9cfe6b47eb34e325f641c950dd29
chris <xristoforospapapos@gmail.com>
2022-11-10 16:55:06 +0000

    added tests

--- RubikCube/src/Main.java
+++ RubikCube/src/Main.java
@@ -1,8 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         Test test = new Test();
+
         test.init(3);
-        //test.testRotation();
-        test.testRotationCol();
+        test.testRotation();
+
+        test.init(3);
+        test.testMidRot();
+
+        test.init(3);
+        test.testBackRot();
+
+        test.init(3);
+        test.testBackRot();
     }
 }
--- RubikCube/src/Test.java
+++ RubikCube/src/Test.java
@@ -10,17 +10,35 @@ public class Test {
         }
 
         public void testRotation() {
+            cube.printCube();
             System.out.println("Testing clockwise rotation");
             cube.turnRow(2, -1);
             cube.printCube();
         }
 
-        public void testRotationCol(){
-            System.out.println("Testing column rotation");
-            cube.turnRow(2, -1);
+        public void testBackRot(){
+            System.out.println("Testing backside rotation");
+            cube.rotateBackSide(1);
             cube.printCube();
-            cube.turnCol(0, 1);
+            cube.rotateBackSide(-1);
             cube.printCube();
-            cube.getBottom().printData();
+        }
+
+        public void testMidRot() {
+            System.out.println("Testing Mid Rotation");
+            cube.rotateMid(1);
+            cube.printCube();
+            cube.rotateMid(1);
+            cube.printCube();
+            cube.rotateMid(1);
+        }
+
+        public void testCompositeRot() {
+            System.out.println("Testing back -> mid -> collumn rotations");
+            cube.rotateMid(-1);
+            cube.printCube();
+            cube.rotateBackSide(-1);
+            cube.printCube();
+            cube.turnCol(2,-1);
         }
 }
